489dcf312b9bae400fe0e73f4bc928c9ec0d9b84
gordonn <tofugordon294@gmail.com>
2025-07-24 20:48:37 -0700

    update

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,42 +2,36 @@
 let darkmode = localStorage.getItem("darkmode");
 const icon = document.getElementById("toggle");
 
-// Set icon depending on mode
 if (darkmode === "active") {
   icon.src = 'img/light-mode.svg';
 } else {
   icon.src = 'img/dark-mode.svg';
 }
 
-// When darkmode is enabled, add the darkmode class to the <body>
 function enableDarkmode() {
   document.documentElement.classList.add("darkmode");
 
-  // Set light mode icon when in dark mode 
   icon.src = 'img/light-mode.svg';
 
   // Save preference in local storage
   localStorage.setItem("darkmode", "active");
 }
 
-// Remove darkmode class from <body>
 function disableDarkmode() {
   document.documentElement.classList.remove("darkmode");
 
-  // Set dark mode icon when in light mode 
   icon.src = 'img/dark-mode.svg';
 
   // Clear the saved preference in local storage
   localStorage.setItem("darkmode", null);
 }
 
-// When the button is clicked:
+// When the button is toggled 
 function themeSwitch() {
 
   // Re-check the current saved theme
   darkmode = localStorage.getItem("darkmode");
 
-  // If it's not "active", enable dark mode; otherwise, disable it (toggle effect).
   if (darkmode !== "active") {
     enableDarkmode();
   } else {
